4b655c62cc332303f456cd5eba321cea40747f14
pky2119 <pky2119@naver.com>
2023-10-29 16:23:15 +0900

    Glide 활용 로고 이미지 로딩

--- app/src/main/java/kr/ac/wku/pizzaorderapp_20231029/adapters/StoreAdapter.kt
+++ app/src/main/java/kr/ac/wku/pizzaorderapp_20231029/adapters/StoreAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.ac.wku.pizzaorderapp_20231029.R
 import kr.ac.wku.pizzaorderapp_20231029.datas.StoreData
 
@@ -36,7 +37,7 @@ class StoreAdapter(
 
         txtStoreName.text = data.name
 
-
+        Glide.with(mContext).load(data.logoURL).into(imgLogo)
 
 
         return row
