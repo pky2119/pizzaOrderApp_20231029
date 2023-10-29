5cd0148412280eb7d58fbf7ed1fa9e16a034adf5
pky2119 <pky2119@naver.com>
2023-10-29 15:49:27 +0900

    피자 가게 리스트 아이템 xml 그리기 까지 -CircleImageView 라이브러리 설치

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "kr.ac.wku.pizzaorderapp_20231029"
-    compileSdk = 33
+    compileSdk = 34
 
     dataBinding{
         enable = true
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "kr.ac.wku.pizzaorderapp_20231029"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -47,4 +47,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
